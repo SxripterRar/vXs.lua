f50855d30fa9b479cbcd6491e7fbb92eb7583fbb
SxripterRar <makassaropenn@gmail.com>
2024-07-28 12:33:00 +0800

    Update Rxjected vX | XD.lua

--- Rxjected vX | XD.lua
+++ Rxjected vX | XD.lua
@@ -1,2 +1,5 @@
 -- Authorized Personel Only.
+task.wait()
+local hi = "LOADED VX XD"
 loadstring(game:HttpGet('https://raw.githubusercontent.com/FlavorYT/The-lazy-ONE/main/Rejected%20VERSION%20X'))()
+print(hi)
