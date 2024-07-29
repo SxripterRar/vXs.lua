6933133ae5d7eb6109b4f451be679dc10024a102
SxripterRar <makassaropenn@gmail.com>
2024-07-29 21:10:27 +0800

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,12 @@
+-- [[what do you think??? Im just testing scripts here]]
+local 24 = 
+game.Workspace:FindFirstChild("whoareyiu24")
+if 24 then
+   print("Hello!")
+else
+   print("Bye!")
+task.wait(1+2)
+elseif not 24 then
+   Game.Players.LocalPlayer:Kick("test")
+   end
+end
