8bb31e8484210e4433186b7c6d59f89a89c7fb92
SxripterRar <makassaropenn@gmail.com>
2024-09-06 19:27:56 +0800

    Create spam.lua

---
+++ spam.lua
@@ -0,0 +1,6 @@
+loopRunning = true
+    
+    while loopRunning do
+    game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("SERVER RAID. | | GET LOST HAHA.", "All")
+    wait(0.25)
+end
