9a70831023146154cfd953c535650487a2055a42
SxripterRar <makassaropenn@gmail.com>
2025-04-06 14:13:04 +0800

    Update troll is a pinning tower.lua

--- troll is a pinning tower.lua
+++ troll is a pinning tower.lua
@@ -25,8 +25,8 @@ barke = state
         wait(0.0001)
          game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new(35.9515839, 4.04425049, 53.7821503, 0, 0, -1, 0, 1, 0, 1, 0, 0)
       end 
-   if barke = false then
-      game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new(-32.3174286, 99.5898972, -8.1511097, 0, 0, -1, 0, 1, 0, 1, 0, 0)
+   if barke == false then
+      game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new(-33.4637527, 104.674164, -10.4693537, -0.999625444, 0.000956700766, -0.0273496751, -5.00574616e-11, 0.999388754, 0.0349589549, 0.0273664035, 0.0349458605, -0.999014437)
       end
 end)
 
