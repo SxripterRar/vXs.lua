90606cf1305d4ecc5afd513c60695b3c700b9fec
SxripterRar <makassaropenn@gmail.com>
2025-04-06 13:34:42 +0800

    Update troll is a pinning tower.lua

--- troll is a pinning tower.lua
+++ troll is a pinning tower.lua
@@ -13,15 +13,16 @@ end)
 Tab1:CreateButton("Teleport to Spawn", function()
    game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new(35.9515839, 4.04425049, 53.7821503, 0, 0, -1, 0, 1, 0, 1, 0, 0)
 end)
-Tab1:CreateToggle("Troll Pads", function(value)
-while value do 
+Tab1:CreateToggle("Troll Pads", function(state)
+barke = state
+      while barke do 
         wait(0.0001)
                   game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new(-34.8450928, 99.5898972, 10.8118877, 0, 0, -1, 0, 1, 0, 1, 0, 0)
         wait(0.0001)
                   game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new(-34.8450928, 99.5899734, 24.9667969, 0, 0, -1, 0, 1, 0, 1, 0, 0)
         wait(0.0001)
                   game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new(-34.8450928, 99.5898972, 38.1106415, 0, 0, -1, 0, 1, 0, 1, 0, 0)
-       end
+end
 end)
 
 Tab2:CreateButton("Nameless Admin", function()
