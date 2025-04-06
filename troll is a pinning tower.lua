196de75be62db6928cbe05edbc77a9f2705967c1
SxripterRar <makassaropenn@gmail.com>
2025-04-06 13:15:01 +0800

    Update troll is a pinning tower.lua

--- troll is a pinning tower.lua
+++ troll is a pinning tower.lua
@@ -7,8 +7,18 @@ local Tab1 = Window:NewSection("Main")
 local Tab2 = Window:NewSection("Misc.")
 
 -- script
-Tab1:CreateToggle("Toggle", function(value)
+Tab1:CreateButton("Teleport to end", function()
+   game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new()
+end)
+Tab1:CreateButton("Teleport to Lobby", function()
+   game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new()
+end)
+Tab1:CreateToggle("Troll Pads", function(value)
 while value do 
-      game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new()
-
+                  game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new(-34.8450928, 99.5898972, 10.8118877, 0, 0, -1, 0, 1, 0, 1, 0, 0)
+        wait(0.1)
+                  game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new(-34.8450928, 99.5899734, 24.9667969, 0, 0, -1, 0, 1, 0, 1, 0, 0)
+        wait(0.2)
+                  game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new(-34.8450928, 99.5898972, 38.1106415, 0, 0, -1, 0, 1, 0, 1, 0, 0)
+       end
 end)
