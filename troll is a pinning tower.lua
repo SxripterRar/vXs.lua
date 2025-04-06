7028b44f869e77b9f331badcc504f2ccdb5672ff
SxripterRar <makassaropenn@gmail.com>
2025-04-06 13:22:33 +0800

    Update troll is a pinning tower.lua

--- troll is a pinning tower.lua
+++ troll is a pinning tower.lua
@@ -5,13 +5,13 @@ local Window = Library:NewWindow("Troll Is A Pinning Tower")
 -- tabs
 local Tab1 = Window:NewSection("Main")
 local Tab2 = Window:NewSection("Misc.")
-
+local Tab3 = Window:NewSection("CREDITS TO RAR.")
 -- script
 Tab1:CreateButton("Teleport to end", function()
-   game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new()
+   game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new(302.850647, 338.706787, -6.88173676, 0, 0, 1, 0, 1, -0, -1, 0, 0)
 end)
-Tab1:CreateButton("Teleport to Lobby", function()
-   game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new()
+Tab1:CreateButton("Teleport to Spawn", function()
+   game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new(35.9515839, 4.04425049, 53.7821503, 0, 0, -1, 0, 1, 0, 1, 0, 0)
 end)
 Tab1:CreateToggle("Troll Pads", function(value)
 while value do 
@@ -21,4 +21,11 @@ while value do
         wait(0.2)
                   game.Players.LocalPlayer.character.HumanoidRootPart.CFrame = CFrame.new(-34.8450928, 99.5898972, 38.1106415, 0, 0, -1, 0, 1, 0, 1, 0, 0)
        end
+end)
+
+Tab2:CreateButton("Nameless Admin", function()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/ltseverydayyou/Nameless-Admin/refs/heads/main/Source.lua"))()
+end)
+Tab2:CreateButton("Infinite Yield", function()
+   loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
 end)
