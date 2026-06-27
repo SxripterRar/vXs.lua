3e9071aee1322a6c1d6afc1d73df5c3ba08af129
SxripterRar <makassaropenn@gmail.com>
2026-06-27 12:58:08 +0800

    Delete Rxjected vX | XD.lua

--- Rxjected vX | XD.lua
+++
@@ -1,5 +0,0 @@
--- Authorized Personel Only.
-task.wait()
-local vx = "LOADED VX XD"
-loadstring(game:HttpGet('https://raw.githubusercontent.com/FlavorYT/The-lazy-ONE/main/Rejected%20VERSION%20X'))()
-game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Enjoy Rxjected vX.", "All")
